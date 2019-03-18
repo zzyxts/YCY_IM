41131a2a5ee092c076b1861431f507983cba0b50
韩毅 <4893214@qq.com>
2019-03-18 20:37:08 +0800

    开创了闪屏页
    desc

---
+++ app/src/main/java/cn/tpddns/FlashActivity.java
@@ -0,0 +1,34 @@
+package cn.tpddns;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class FlashActivity extends AppCompatActivity {
+    private Handler mHandler;
+    private Intent mIntent;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+        init();
+    }
+
+    private void init() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(FlashActivity.this, MainActivity.class));
+            }
+
+        }).start();
+    }
+}
